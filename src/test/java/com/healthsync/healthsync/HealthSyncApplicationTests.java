94b1814f1383aada940b85859832ef0ca98fff5b
Ritu Raj Singh <aravkumarsingh1998@gmail.com>
2026-01-10 14:38:03 +0530

    initial commit

---
+++ src/test/java/com/healthsync/healthsync/HealthSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthsync.healthsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
