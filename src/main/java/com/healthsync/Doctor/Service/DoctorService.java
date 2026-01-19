ad73558815cf12a8504479c20532178f2d0e25b8
Ritu Raj Singh <aravkumarsingh1998@gmail.com>
2026-01-19 23:11:18 +0530

    commit changes

--- src/main/java/com/healthsync/Doctor/Service/DoctorService.java
+++ src/main/java/com/healthsync/Doctor/Service/DoctorService.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 public class DoctorService {
     @Autowired
-    DoctorRepo doctorRepo;
+   private DoctorRepo doctorRepo;
     public ResponseEntity<Object> createDoctor(Doctor doctor) {
 
         if (doctor.getMobileNumber() < 1000000000L || doctor.getMobileNumber() > 9999999999L) {
